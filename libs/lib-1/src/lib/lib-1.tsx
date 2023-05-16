5ec612833cf8e1c6999bec37c0480401f07bb34f
Akis ARou <akisarou90@gmail.com>
2023-05-17 00:35:56 +0300

    chore: rename back to tsconfig.base.json

--- libs/lib-1/src/lib/lib-1.tsx
+++ libs/lib-1/src/lib/lib-1.tsx
@@ -1,11 +1,6 @@
-import styles from './lib-1.module.css';
-
-/* eslint-disable-next-line */
-export interface Lib1Props {}
-
-export function Lib1(props: Lib1Props) {
+export function Lib1() {
   return (
-    <div className={styles['container']}>
+    <div>
       <h1>Welcome to Lib1!</h1>
     </div>
   );
--- libs/lib-2/src/lib/lib-2.tsx
+++ libs/lib-2/src/lib/lib-2.tsx
@@ -1,13 +1,9 @@
 import { Lib1 } from '@nx-nvim/lib-1';
-import styles from './lib-2.module.css';
 
-/* eslint-disable-next-line */
-export interface Lib2Props {}
-
-export function Lib2(props: Lib2Props) {
+export function Lib2() {
   return (
-    <div className={styles['container']}>
-      <Lib1/>
+    <div>
+      <Lib1 />
       <h1>Welcome to Lib2!</h1>
     </div>
   );
